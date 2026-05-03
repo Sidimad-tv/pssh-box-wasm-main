dda4313dcb6c87f766dda7c511d703d953464ac5
SidimaD <136781211+Sidimad-tv@users.noreply.github.com>
2026-05-03 06:39:07 +0100

    Add files via upload

---
+++ pkg/pssh_box_wasm_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const code_version: () => [number, number];
+export const fetch_pssh_data: (a: number, b: number) => any;
+export const generate_widevine_pssh_b64: (a: number, b: any, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number) => [number, number, number, number];
+export const pssh_base64_to_html: (a: number, b: number) => [number, number, number, number];
+export const pssh_hex_to_html: (a: number, b: number) => [number, number, number, number];
+export const wasm_bindgen__convert__closures_____invoke__heebdcfb6ef334ed7: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h56932e970e24db41: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
