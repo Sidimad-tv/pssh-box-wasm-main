9b4de76c9001e94d1d54441ddb3a2c730f68c2cf
Sidimad-tv <imad.sidi4@gmail.com>
2026-05-09 02:54:56 +0100

    Use latest construct from PyPI instead of tar.gz

--- www-zola/static/js/get-license.js
+++ www-zola/static/js/get-license.js
@@ -8,7 +8,7 @@ init().then(() => {
 const myPackages = [
     "pyodide_http",
     "requests",
-    "https://files.pythonhosted.org/packages/b6/2c/66bab4fef920ef8caa3e180ea601475b2cbbe196255b18f1c58215940607/construct-2.8.8.tar.gz",
+    "construct",
     "https://files.pythonhosted.org/packages/aa/a2/27fea39af627c0ce5dbf6108bf969ea8f5fc9376d29f11282a80e3426f1d/pymp4-1.4.0-py3-none-any.whl",
     "https://files.pythonhosted.org/packages/41/9f/60f8a4c8e7767a8c34f5c42428662e03fa3e38ad18ba41fcc5370ee43263/pywidevine-1.8.0-py3-none-any.whl"
 ];
