4459cd89d67b74293ce401e1b65adddf8a2799a3
Sidimad-tv <imad.sidi4@gmail.com>
2026-05-09 02:51:53 +0100

    Use micropip.install for package loading in get-license.js

--- www-zola/static/js/get-license.js
+++ www-zola/static/js/get-license.js
@@ -21,7 +21,9 @@ const myPackages = [
     "https://files.pythonhosted.org/packages/41/9f/60f8a4c8e7767a8c34f5c42428662e03fa3e38ad18ba41fcc5370ee43263/pywidevine-1.8.0-py3-none-any.whl",
     "https://files.pythonhosted.org/packages/aa/a2/27fea39af627c0ce5dbf6108bf969ea8f5fc9376d29f11282a80e3426f1d/pymp4-1.4.0-py3-none-any.whl"
 ];
-let pyodide = await loadPyodide({ packages: myPackages });
+let pyodide = await loadPyodide();
+await pyodide.loadPackage("micropip");
+await pyodide.pyimport("micropip").install(myPackages);
 console.log("Pyodide + pywidevine loaded");
 document.getElementById("loading").style.display = "none";
 if (typeof pyodide.setDebug === 'function') {
